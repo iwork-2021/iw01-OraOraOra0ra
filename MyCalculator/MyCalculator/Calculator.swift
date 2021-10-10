299f0bba89d8e87a5ed2b6084219e31be9b1a199
OraOraOra0ra <1994619908@qq.com>
2021-10-10 20:48:41 +0800

    code push

--- MyCalculator/MyCalculator/Calculator.swift
+++ MyCalculator/MyCalculator/Calculator.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+var is_Rad = true
+
 class Calculator: NSObject {
     enum Operation {
         case UnaryOp((Double)->Double)
@@ -51,6 +53,244 @@ class Calculator: NSObject {
         "AC": Operation.UnaryOp {
             _ in
             return 0
+        },
+        
+        "x²": Operation.UnaryOp {
+            op in
+            return pow(op,2)
+        },
+        
+        "x³": Operation.UnaryOp {
+            op in
+            return pow(op,3)
+        },
+        
+        "xʸ": Operation.BinaryOp {
+            op1,op2 in
+            return pow(op1,op2)
+        },
+        
+        "yˣ": Operation.BinaryOp {
+            op1,op2 in
+            return pow(op2,op1)
+        },
+        
+        "eˣ": Operation.UnaryOp {
+            op in
+            return exp(op)
+        },
+        
+        "2ˣ": Operation.UnaryOp {
+            op in
+            return pow(2,op)
+        },
+        
+        "10ˣ": Operation.UnaryOp {
+            op in
+            return pow(10,op)
+        },
+        
+        "1/x": Operation.UnaryOp {
+            op in
+            return 1.0/op
+        },
+        
+        "²√x": Operation.UnaryOp {
+            op in
+            return sqrt(op)
+        },
+        
+        "³√x": Operation.UnaryOp {
+            op in
+            return cbrt(op)
+        },
+        
+        "ʸ√x":Operation.BinaryOp {
+            op1,op2 in
+            return pow(op1,1.0/op2)
+        },
+        
+        "ln": Operation.UnaryOp {
+            op in
+            return log(op)
+        },
+        
+        "logy": Operation.BinaryOp {
+            op1,op2 in
+            return log(op1)/log(op2)
+        },
+        
+        "log₁₀": Operation.UnaryOp {
+            op in
+            return log10(op)
+        },
+        
+        "log2": Operation.UnaryOp {
+            op in
+            return log2(op)
+        },
+        
+        "x!": Operation.UnaryOp {
+            op in
+            var tmp = op
+            var res = 1.0
+            while (tmp > 1){
+                res *= tmp
+                tmp -= 1.0
+            }
+            return res
+        },
+        
+        "sin": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return sin(op)
+            }
+            else{
+                return sin(op/180.0 * .pi)
+            }
+        },
+        
+        "asin": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return asin(op)
+            }
+            else{
+                return asin(op/180.0 * .pi)
+            }
+        },
+        
+        "cos": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return cos(op)
+            }
+            else{
+                return cos(op/180.0 * .pi)
+            }
+        },
+        
+        "acos": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return acos(op)
+            }
+            else{
+                return acos(op/180.0 * .pi)
+            }
+        },
+        
+        "tan": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return tan(op)
+            }
+            else{
+                return tan(op/180.0 * .pi)
+            }
+        },
+        
+        "atan": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return atan(op)
+            }
+            else{
+                return atan(op/180.0 * .pi)
+            }
+        },
+        
+        "e": Operation.UnaryOp {
+            op in
+            return exp(1)
+        },
+        
+        "EE": Operation.BinaryOp {
+            op1, op2 in
+            return op1 * pow(10,op2)
+        },
+        
+        "sinh": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return sinh(op)
+            }
+            else{
+                return sinh(op/180.0 * .pi)
+            }
+        },
+        
+        "asinh": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return asinh(op)
+            }
+            else{
+                return asinh(op/180.0 * .pi)
+            }
+        },
+        
+        "cosh": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return cosh(op)
+            }
+            else{
+                return cosh(op/180.0 * .pi)
+            }
+        },
+        
+        "acosh": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return acosh(op)
+            }
+            else{
+                return acosh(op/180.0 * .pi)
+            }
+        },
+        
+        "tanh": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return tanh(op)
+            }
+            else{
+                return tanh(op/180.0 * .pi)
+            }
+        },
+        
+        "atanh": Operation.UnaryOp {
+            op in
+            if is_Rad{
+                return atanh(op)
+            }
+            else{
+                return atanh(op/180.0 * .pi)
+            }
+        },
+        
+        "π": Operation.UnaryOp {
+            op in
+            return .pi
+        },
+        
+        "Rand": Operation.UnaryOp {
+            op in
+            return drand48()
+        },
+        
+        "Rad": Operation.UnaryOp {
+            op in
+            is_Rad = !is_Rad
+            return op
+        },
+        
+        "Deg": Operation.UnaryOp {
+            op in
+            is_Rad = !is_Rad
+            return op
         }
     ]
     
@@ -69,7 +309,14 @@ class Calculator: NSObject {
             case .Constant(let value):
                 return value
             case .EqualOp:
-                return pendingOp!.waitingOperation(pendingOp!.firstOp,operand)
+                if pendingOp == nil{
+                    return operand
+                }
+                else{
+                    let res = pendingOp!.waitingOperation(pendingOp!.firstOp,operand)
+                    pendingOp = nil
+                    return res
+                }
             case .UnaryOp(let function):
                 return function(operand)
             }
--- MyCalculator/MyCalculator/ViewController.swift
+++ MyCalculator/MyCalculator/ViewController.swift
@@ -49,10 +49,26 @@ class ViewController: UIViewController {
     
     let calculator = Calculator()
     
+    @IBOutlet weak var Rad_Deg: UIButton!
+    
     @IBAction func operatorTouched(_ sender: UIButton) {
         if let op = sender.currentTitle {
+            if op == "Rad"{
+                Rad_Deg.setTitle("Deg", for: .normal)
+            }
+            else if op == "Deg"{
+                Rad_Deg.setTitle("Rad", for: .normal)
+            }
+            
             if let result = calculator.performOperation(operation: op, operand: Double(digitOnDisplay)!) {
-                digitOnDisplay = String(result)
+                let tmp = String(round(1e13*result)/1e13)
+                let res = Double(tmp)
+                if res! < 1E18 && Double(Int(result)) == result{
+                    digitOnDisplay = String(Int(res!))
+                }
+                else{
+                    digitOnDisplay = String(res!)
+                }
             }
             
             inTypingMode = false
@@ -61,5 +77,46 @@ class ViewController: UIViewController {
     }
     
     
+    
+    @IBOutlet weak var e2y: UIButton!
+    @IBOutlet weak var ten2two: UIButton!
+    @IBOutlet weak var ln2logy: UIButton!
+    @IBOutlet weak var two2ten: UIButton!
+    @IBOutlet weak var sin: UIButton!
+    @IBOutlet weak var cos: UIButton!
+    @IBOutlet weak var tan: UIButton!
+    @IBOutlet weak var sinh: UIButton!
+    @IBOutlet weak var cosh: UIButton!
+    @IBOutlet weak var tanh: UIButton!
+        
+    var flag = false
+    @IBAction func Swap(_ sender: UIButton) {
+        if flag {
+            e2y.setTitle("eˣ", for: .normal)
+            ten2two.setTitle("10ˣ", for: .normal)
+            ln2logy.setTitle("ln", for: .normal)
+            two2ten.setTitle("log₁₀", for: .normal)
+            sin.setTitle("sin", for: .normal)
+            cos.setTitle("cos", for: .normal)
+            tan.setTitle("tan", for: .normal)
+            sinh.setTitle("sinh", for: .normal)
+            cosh.setTitle("cosh", for: .normal)
+            tanh.setTitle("tanh", for: .normal)
+            flag = false
+        }
+        else{
+            e2y.setTitle("yˣ", for: .normal)
+            ten2two.setTitle("2ˣ", for: .normal)
+            ln2logy.setTitle("logy", for: .normal)
+            two2ten.setTitle("log2", for: .normal)
+            sin.setTitle("asin", for: .normal)
+            cos.setTitle("acos", for: .normal)
+            tan.setTitle("atan", for: .normal)
+            sinh.setTitle("asinh", for: .normal)
+            cosh.setTitle("acosh", for: .normal)
+            tanh.setTitle("atanh", for: .normal)
+            flag = true
+        }
+    }
 }
 
